c4b76d1bd6ac88007a568e79a89554171320b8e2
Ekaterina <miteneva.kate@gmail.com>
2021-12-10 22:48:43 +0300

    popup show and close

--- src/view/films-list-view.js
+++ src/view/films-list-view.js
@@ -2,6 +2,9 @@ import {createElement} from '../render.js';
 
 const createFilmsListTemplate = () => (
   `<section class="films-list">
+    <div class="films-list__container">
+      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+    </div>
   </section>`
 );
 
